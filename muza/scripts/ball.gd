c365691f6ae1c3e57d05eed99b2a36f9f4d5e62a
gabriel <gabrielc@eng.uerj.br>
2025-09-26 15:14:08 -0300

    Sapo atira bolas corretamente

--- muza/scripts/ball.gd
+++ muza/scripts/ball.gd
@@ -1,9 +1,27 @@
-extends Node2D
+extends CharacterBody2D
 class_name Ball
 
 enum Colors{YELLOW = 0xffffffff, RED = 0xff1c76ff, GREEN = 0x00ff00ff}
 
+@export var shot_speed : int = 500
 @export var color : Colors = Colors.YELLOW:
 	set(value):
 		color = value
 		self.modulate = color
+
+var _is_shot : bool = false
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+
+func be_shot(at_point : Vector2) -> void:
+	if _is_shot:
+		return
+	var direction_shot : Vector2 = (at_point-global_position).normalized()
+	_is_shot = true
+	velocity = direction_shot*shot_speed
+
+func stop() -> void:
+	_is_shot = false
+	velocity = Vector2.ZERO
+	
--- muza/scripts/frog.gd
+++ muza/scripts/frog.gd
@@ -11,16 +11,29 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_released("ui_accept"):
+	if Input.is_action_just_released("swap_balls"):
 		swap_balls()
+	if Input.is_action_just_released("shoot"):
+		shoot(get_global_mouse_position())
+
+
+func give_ball(from_node : Node, to_node : Node) -> void:
+	var ball : Ball = from_node.get_child(0)
+	from_node.remove_child(ball)
+	to_node.add_child(ball)
 
 func swap_balls() -> void:
-	var mouth_ball : Ball = ready_ball_position.get_child(0)
-	var other_ball : Ball = stand_by_ball_position.get_child(0)
-	ready_ball_position.remove_child(mouth_ball)
-	stand_by_ball_position.add_child(mouth_ball)
-	stand_by_ball_position.remove_child(other_ball)
-	ready_ball_position.add_child(other_ball)
-	mouth_ball.position = Vector2.ZERO
+	give_ball(ready_ball_position,stand_by_ball_position)
+	give_ball(stand_by_ball_position,ready_ball_position)
 
-	other_ball.position = Vector2.ZERO
+	
+func shoot(at_point : Vector2) -> void:
+	var mouth_ball : Ball = ready_ball_position.get_child(0)
+	var original_position : Vector2 = mouth_ball.global_position
+	ready_ball_position.remove_child(mouth_ball)
+	get_parent().add_child(mouth_ball)
+	mouth_ball.global_position = original_position
+	mouth_ball.be_shot(at_point)
+	
+	give_ball(stand_by_ball_position,ready_ball_position)
+	stand_by_ball_position.add_child(ball_spawner.spawn_except(ready_ball_position.get_child(0).color))
