ee5c17eebb687fe62f17f3ee12afc3e656c4368e
gabriel <gabrielc@eng.uerj.br>
2025-10-03 16:27:45 -0300

    Level termina quando bolas chegam ao final. O commit anterior também garante o spawn independente das bolas do sapo

--- muza/scripts/ball_path.gd
+++ muza/scripts/ball_path.gd
@@ -6,14 +6,19 @@ class_name BallPath
 @onready var begining_checker: Area2D = $BeginingChecker
 
 @export_range(1,100000,1) var speed : int = 50
+@export_range(1,100000,1) var end_of_level_speed : int = 400
 
 var number_of_balls_in_path : int = 0
 var _can_spawn : bool = true
+var _level_ended : bool = false
 
 func _ready() -> void:
 	begining_checker.position = path.curve.get_baked_points()[0]
 
 func _process(delta: float) -> void:
+	if _level_ended:
+		end_of_level()
+		return 
 	if _can_spawn:
 		put_ball_on_path()
 
@@ -22,6 +27,9 @@ func _physics_process(delta: float) -> void:
 		path_follow.progress += speed*delta
 		if path_follow.progress_ratio == 1.0:
 			handle_ball_reached_the_end(path_follow)
+
+func end_of_level():
+	speed = end_of_level_speed
 
 func put_ball_on_path() -> void:
 	_can_spawn = false
@@ -32,8 +40,11 @@ func put_ball_on_path() -> void:
 	number_of_balls_in_path = path.get_child_count()
 
 func handle_ball_reached_the_end(path_follow : PathFollow2D):
+	_level_ended = true
 	path_follow.queue_free()
 	number_of_balls_in_path = path.get_child_count()
+	if number_of_balls_in_path == 1:
+		print(path.get_child_count())
 
 func _on_begining_checker_area_exited(area: Area2D) -> void:
 	if area.get_parent().ball_owner != Ball.Owner.PATH:
